2bb18e5663a60f0ac428d1dfcf1d842ec9f86f62
cyb <2103561941@qq.com>
2023-01-21 13:16:24 +0800

    feat(rpc/video):完成 GetCommentList 接口

--- service/rpc/video/internal/logic/getCommentListLogic.go
+++ service/rpc/video/internal/logic/getCommentListLogic.go
@@ -2,6 +2,9 @@ package logic
 
 import (
 	"context"
+	"google.golang.org/grpc/status"
+	"h68u-tiktok-app-microservice/common/error/rpcErr"
+	"h68u-tiktok-app-microservice/common/model"
 	"h68u-tiktok-app-microservice/service/rpc/video/internal/svc"
 	"h68u-tiktok-app-microservice/service/rpc/video/types/video"
 
@@ -23,7 +26,27 @@ func NewGetCommentListLogic(ctx context.Context, svcCtx *svc.ServiceContext) *Ge
 }
 
 func (l *GetCommentListLogic) GetCommentList(in *video.GetCommentListRequest) (*video.GetCommentListResponse, error) {
-	// todo: add your logic here and delete this line
+	// 按倒序获取视频的评论列表
+	var comments []model.Comment
+	if err := l.svcCtx.DBList.Mysql.
+		Where("video_id = ?", in.VideoId).
+		Order("created_at").
+		Find(&comments).Error; err != nil {
+		return nil, status.Error(rpcErr.DataBaseError.Code, err.Error())
+	}
 
-	return &video.GetCommentListResponse{}, nil
+	// 封装评论数据
+	commentList := make([]*video.Comment, 0, len(comments))
+	for _, v := range comments {
+		commentList = append(commentList, &video.Comment{
+			Id:         int32(v.ID),
+			AuthorId:   int32(v.UserId),
+			CreateTime: int32(v.CreatedAt.Unix()),
+			Content:    v.Content,
+		})
+	}
+
+	return &video.GetCommentListResponse{
+		CommentList: commentList,
+	}, nil
 }
