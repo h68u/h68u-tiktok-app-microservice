a0fe1651d21d5feffd4bd3a6aec7907f4aed8628
B-Zz <m15867689095@163.com>
2023-01-21 16:28:51 +0800

    完善GetVideoList Rpc

--- service/rpc/video/internal/logic/getVideoListLogic.go
+++ service/rpc/video/internal/logic/getVideoListLogic.go
@@ -2,6 +2,9 @@ package logic
 
 import (
 	"context"
+	"google.golang.org/grpc/status"
+	"h68u-tiktok-app-microservice/common/error/rpcErr"
+	"h68u-tiktok-app-microservice/common/model"
 	"h68u-tiktok-app-microservice/service/rpc/video/internal/svc"
 	"h68u-tiktok-app-microservice/service/rpc/video/types/video"
 
@@ -23,7 +26,27 @@ func NewGetVideoListLogic(ctx context.Context, svcCtx *svc.ServiceContext) *GetV
 }
 
 func (l *GetVideoListLogic) GetVideoList(in *video.GetVideoListRequest) (*video.GetVideoListResponse, error) {
-	// todo: add your logic here and delete this line
+	var videos []model.Video
 
-	return &video.GetVideoListResponse{}, nil
+	err := l.svcCtx.DBList.Mysql.Order("created_at desc").Limit(int(in.Num)).Find(&videos).Error
+	if  err != nil {
+		return nil, status.Error(rpcErr.DataBaseError.Code, err.Error())
+	}
+
+	var videoList  []*video.VideoInfo
+	for _, v := range videos {
+		videoList = append(videoList, &video.VideoInfo{
+			Id:            int32(v.ID),
+			AuthorId:      int32(v.AuthorId),
+			Title:         v.Title,
+			PlayUrl:       v.PlayUrl,
+			CoverUrl:      v.CoverUrl,
+			FavoriteCount: int32(v.FavoriteCount),
+			CommentCount:  int32(v.CommentCount),
+		})
+	}
+
+	return &video.GetVideoListResponse{
+		VideoList: videoList,
+	}, nil
 }
