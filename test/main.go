be3fd1b9df5695744a7b89413f5f9806afd2a119
cyb <2103561941@qq.com>
2023-02-19 14:24:45 +0800

    test: test reviewdog in github actions when pull request

--- test/main.go
+++ test/main.go
@@ -1,7 +1,29 @@
 package main
 
+import (
+	"fmt"
+	"sync"
+)
+
 func main() {
-	FuncA() // without error check
+
+	arr := []int{1, 2, 3, 4}
+
+	var wg sync.WaitGroup
+	wg.Add(len(arr))
+	for _, v := range arr {
+		go func ()  {
+			defer wg.Done()
+			fmt.Println(v)
+		}()
+	}
+
+
+	// without error check
+	FuncA() 
+
+
+	wg.Wait()
 }
 
 func FuncA() error {
